f9d7b120074c90f844a6b0ab52d6f6d4d2a5b267
Sunny <chojnacki.mirek@gmail.com>
2023-02-15 21:33:31 +0100

    create displayworkorder lua file

---
+++ minecolonies/displayworkorder.lua
@@ -0,0 +1,35 @@
+function init (monitorPeripheralSide, integratorPeripheralSide)
+    monitor = peripheral.wrap(monitorPeripheralSide) -- monitor
+    ci = peripheral.wrap(integratorPeripheralSide) -- colony integrator
+    return monitor, ci
+end
+
+
+function getFirstOrder (colonyIntegrator)
+    ci = colonyIntegrator
+    workOrders = ci.getWorkOrders()
+    if table.getn(workOrders) > 0 then
+        orderTable = workOrders[1]
+        orderResourcesTable = ci.getWorkOrderResources(order.orderTable["id"])
+        builderResources = ci.getBuilderResources(orderTable["builder"]["x"], orderTable["builder"]["y"], orderTable["builder"]["z"])
+    end
+    return orderTable, orderResourcesTable, builderResources
+end
+
+function monitorWrite (monitor, orderTable, orderResourcesTable, builderResources)
+    mo = monitor
+    mo.clear()
+    mo.setCursorPos(0, 0)
+    mo.write("Type:", orderTable["workOrderType"], "Name:", orderTable["buildingName"], "Type:", orderTable["type"], "Level:", orderTable["targetLevel"])
+    mo.write("Name                " + "Needed", "Delivered")
+    for k,v in ipairs(orderResourcesTable) do\
+        mo.write(orderResourcesTable["displayName"], orderResourcesTable["needed"])
+    end
+end
+
+mo, ci = init(monitorPeripheralSide="left", integratorPeripheralSide="right")
+
+while true do
+    monitorWrite(mo, getFirstOrder())
+    sleep(1)
+end
