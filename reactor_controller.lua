3ed7a6c62e66050eeb66e7993d48a5fa8187ac34
Sunny <sunnythealpha@gmail.com>
2024-12-29 00:30:12 +0100

    update reactor controller

---
+++ reactor_controller.lua
@@ -0,0 +1,37 @@
+function failsafe_check(side, name)
+    -- sensor = peripheral.wrap(side)
+    if redstone.getAnalogInput(side) > 0 then
+        print(name + " failsafe triggered!")
+        return true
+    end
+end
+
+function enable_reactor()
+    if not get_reactor_switch_state() then
+        print("enabling reactor!")
+        redstone.setAnalogOutput("back", 15)
+    end
+end
+
+function disable_reactor()
+    if get_reactor_switch_state() then
+        print("disabling reactor!")
+        redstone.setAnalogOutput("back", 0)
+    end
+end
+
+function get_reactor_switch_state()
+    if (redstone.getAnalogOutput("back") > 0) then
+        return true
+    end
+end
+
+while true do
+    temp_sensor = failsafe_check("left", "temperature")
+    waste_sensor = failsafe_check("top", "waste")
+    damage_sensor = failsafe_check("right", "damage")
+
+    if temp_sensor or waste_sensor or damage_sensor then
+        disable_reactor()
+    end
+end
