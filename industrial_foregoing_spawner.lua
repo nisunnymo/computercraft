6b1df1a13a13dd1214ee4042a76d3ac37394b9a1
Sunny <sunnythealpha@gmail.com>
2025-06-20 22:21:52 +0200

    adding comments

--- industrial_foregoing_spawner.lua
+++ industrial_foregoing_spawner.lua
@@ -6,22 +6,26 @@ local crusher_switch = peripheral.wrap("right")
 local spawner_switch = peripheral.wrap("redstone_relay_5")
 while true do
     if redstone.getAnalogInput("front") > 0 then
+        print("enabling spawner from ae2 input")
         -- enabling spawner from ae2 redstone input
         spawner.setEnabled("top", true)
         redstone.setAnalogOutput("left", 15)
         crusher.setEnabled("right", false)
     elseif slaugher_factory_switch.getInput("front") then
-        -- enabling slaughter factory and crusher from manual input
+        -- enabling slaughter factory from manual input
+        print("enabling crusher from manual input")
         spawner.setEnabled("top", true)
         redstone.setAnalogOutput("left", 15)
         crusher.setEnabled("right", false)
     elseif crusher_switch.getInput("front") then
-        -- enabling spawner and crusher from manual input
+        -- enabling crusher from manual input
+        print("enabling crusher from manual input")
         spawner.setEnabled("top", true)
         crusher.setEnabled("right", true)
         redstone.setAnalogOutput("left", 0)
     elseif spawner_switch.getInput("front") then
         -- enabling spawner only from manual input
+        print("enabling spawner only from manual input")
         spawner.setEnabled("top", true)
         redstone.setAnalogOutput("left", 0)
         crusher.setEnabled("right", false)
